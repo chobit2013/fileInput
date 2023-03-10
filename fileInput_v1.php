0350cd8d5adf32ad1cef7d9dded71e388aa17c35
unknown <User@HTG-09012-NB.hota.com.tw>
2023-03-10 16:19:07 +0800

    第四次上傳

--- fileInput_v1.php
+++ fileInput_v1.php
@@ -10,18 +10,14 @@ $contents = fread($myfile, filesize($filename));
 $charTab_row = explode("\n", $contents);
 //字串拆行，轉陣列
 
-
 foreach ($charTab_row as $item => $value) {
     $charTab_row[$item] = explode("\t", $value);
-    $charTab_row[$item][0] = "";
-    //刪除$charTab_row[$item][0]
-
-    var_dump($charTab_row);
 
     unset($charTab_row[$item][1]);
     //刪除第一行
     unset($charTab_row[$item][2]);
     //刪除第二行
+
 }
 
 
